c10cb716ba775b3ff16af7bd32dc425bac76d4c5
JoaoM359 <359.joao@gmail.com>
2026-01-10 12:43:27 +0000

    Added MoviesRepository initial implementation

--- composeApp/src/commonMain/kotlin/com/example/movies/domain/model/Movie.kt
+++ composeApp/src/commonMain/kotlin/com/example/movies/domain/model/Movie.kt
@@ -1,10 +1,20 @@
 package com.example.movies.domain.model
 
+import com.example.movies.data.network.IMAGE_SMALL_BASE_URL
+import com.example.movies.data.network.model.MoviesResponse
+
 data class Movie(
     val id: Int,
     val title: String,
     val overview: String,
     val posterUrl: String
+)
+
+fun MoviesResponse.toModel() = Movie(
+    id = this.id,
+    title = this.title,
+    overview = this.overview,
+    posterUrl = "${IMAGE_SMALL_BASE_URL}${this.posterPath}"
 )
 
 // Fake objects
