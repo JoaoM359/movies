56ae49277ac36d545dd033cb383f9b00761651bc
JoaoM359 <359.joao@gmail.com>
2026-01-12 21:34:04 +0000

    Refactoring MoviesListScreen with state handling

--- composeApp/src/androidMain/kotlin/com/example/movies/previews/MoviesPreview.kt
+++ composeApp/src/androidMain/kotlin/com/example/movies/previews/MoviesPreview.kt
@@ -2,9 +2,11 @@ package com.example.movies.previews
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.movies.domain.model.MovieSection
 import com.example.movies.domain.model.movie1
 import com.example.movies.ui.components.MoviePoster
 import com.example.movies.ui.movies.MoviesListScreen
+import com.example.movies.ui.movies.MoviesListViewModel.MoviesListState
 
 @Preview(showBackground = true)
 @Composable
@@ -12,4 +14,25 @@ private fun MoviePosterPreview() = MoviePoster(movie = movie1)
 
 @Preview
 @Composable
-private fun MoviesListScreenPreview() = MoviesListScreen(popularMovies = listOf(movie1))
+private fun MoviesListScreenSuccessPreview() = MoviesListScreen(
+    moviesListState = MoviesListState.Success(
+        movieSection = listOf(
+            MovieSection(
+                sectionType = MovieSection.SectionType.POPULAR,
+                movies = listOf(movie1)
+            )
+        )
+    )
+)
+
+@Preview
+@Composable
+private fun MoviesListScreenErrorPreview() = MoviesListScreen(
+    moviesListState = MoviesListState.Error("Error")
+)
+
+@Preview
+@Composable
+private fun MoviesListScreenLoadingPreview() = MoviesListScreen(
+    moviesListState = MoviesListState.Loading
+)
--- composeApp/src/commonMain/kotlin/com/example/movies/ui/movies/MoviesListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/movies/ui/movies/MoviesListScreen.kt
@@ -1,71 +1,75 @@
 package com.example.movies.ui.movies
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.movies.data.network.IMAGE_SMALL_BASE_URL
-import com.example.movies.data.network.KtorClient
-import com.example.movies.domain.model.Movie
-import com.example.movies.domain.model.movie1
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.movies.data.repository.MoviesRepository
+import com.example.movies.domain.model.MovieSection
 import com.example.movies.ui.components.MoviesSection
 
 @Composable
-fun MoviesListRoute() {
-    var popularMovies by remember { mutableStateOf(emptyList<Movie>()) }
-    LaunchedEffect(Unit) {
-        val response = KtorClient.getMovies("popular")
-        popularMovies = response.results.map { movieResponse ->
-            Movie(
-                id = movieResponse.id,
-                title = movieResponse.title,
-                overview = movieResponse.overview,
-                posterUrl = "${IMAGE_SMALL_BASE_URL}${movieResponse.posterPath}"
-            )
-        }
+fun MoviesListRoute(
+    viewModel: MoviesListViewModel = viewModel {
+        MoviesListViewModel(moviesRepository = MoviesRepository())
     }
-
-    MoviesListScreen(popularMovies = popularMovies)
+) {
+    val moviesListState by viewModel.moviesListState.collectAsStateWithLifecycle()
+    MoviesListScreen(moviesListState = moviesListState)
 }
 
 @Composable
 fun MoviesListScreen(
-    popularMovies: List<Movie>
+    moviesListState: MoviesListViewModel.MoviesListState
 ) {
-    Scaffold {
-        LazyColumn(
-            modifier = Modifier.padding(it),
-            contentPadding = PaddingValues(vertical = 16.dp)
+    Scaffold { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
         ) {
-            item {
-                MoviesSection(
-                    title = "Popular Movies",
-                    movies = popularMovies
-                )
-            }
+            when (moviesListState) {
+                MoviesListViewModel.MoviesListState.Loading -> {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
 
-            item {
-                MoviesSection(
-                    title = "Top Rated Movies",
-                    movies = List(10) { movie1 },
-                    modifier = Modifier.padding(top = 32.dp)
-                )
-            }
+                is MoviesListViewModel.MoviesListState.Success -> {
+                    LazyColumn(
+                        contentPadding = PaddingValues(vertical = 16.dp),
+                        verticalArrangement = Arrangement.spacedBy(32.dp)
+                    ) {
+                        items(moviesListState.movieSection) { movieSection ->
+                            val title = when (movieSection.sectionType) {
+                                MovieSection.SectionType.POPULAR -> "Popular Movies"
+                                MovieSection.SectionType.TOP_RATED -> "Top Rated Movies"
+                                MovieSection.SectionType.UPCOMING -> "Upcoming Movies"
+                            }
+                            MoviesSection(title = title, movies = movieSection.movies)
+                        }
+                    }
+                }
 
-            item {
-                MoviesSection(
-                    title = "Upcoming Movies",
-                    movies = List(10) { movie1 },
-                    modifier = Modifier.padding(top = 32.dp)
-                )
+                is MoviesListViewModel.MoviesListState.Error -> {
+                    Text(
+                        text = moviesListState.message,
+                        modifier = Modifier.align(Alignment.Center).padding(16.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
         }
     }
