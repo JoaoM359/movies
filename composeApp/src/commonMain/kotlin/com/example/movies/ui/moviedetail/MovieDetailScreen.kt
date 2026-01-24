ae28067652cddc22d1292a80ca7f30d22789f9d8
JoaoM359 <359.joao@gmail.com>
2026-01-24 14:04:53 +0000

    Built MovieDetailScreen structure

--- composeApp/src/commonMain/kotlin/com/example/movies/ui/moviedetail/MovieDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/movies/ui/moviedetail/MovieDetailScreen.kt
@@ -1,10 +1,22 @@
 package com.example.movies.ui.moviedetail
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,14 +26,29 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale.Companion.Crop
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.movies.domain.model.Movie
 import com.example.movies.domain.model.movie1
+import com.example.movies.ui.components.CastMemberItem
+import com.example.movies.ui.components.MovieGenreChip
+import com.example.movies.ui.components.MovieInfoItem
 import com.example.movies.ui.theme.MoviesAppTheme
 import compose.icons.FontAwesomeIcons
 import compose.icons.fontawesomeicons.Solid
 import compose.icons.fontawesomeicons.solid.ArrowLeft
+import compose.icons.fontawesomeicons.solid.Calendar
+import compose.icons.fontawesomeicons.solid.Clock
+import compose.icons.fontawesomeicons.solid.Play
+import compose.icons.fontawesomeicons.solid.Star
+import movies.composeapp.generated.resources.Res
+import movies.composeapp.generated.resources.minecraft_movie
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -64,6 +91,129 @@ fun MovieDetailScreen(movie: Movie) {
                 .fillMaxSize()
                 .padding(paddingValues = paddingValues)
         ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .weight(1f),
+                shape = MaterialTheme.shapes.large
+            ) {
+                Image(
+                    painter = painterResource(Res.drawable.minecraft_movie),
+                    contentDescription = null,
+                    modifier = Modifier.clip(MaterialTheme.shapes.large),
+                    contentScale = Crop
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
+                    .weight(2f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = movie.title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    // Rating
+                    MovieInfoItem(
+                        icon = FontAwesomeIcons.Solid.Star,
+                        text = "7.5"
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    // Duration
+                    MovieInfoItem(
+                        icon = FontAwesomeIcons.Solid.Clock,
+                        text = "2h 36min"
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    // Year
+                    MovieInfoItem(
+                        icon = FontAwesomeIcons.Solid.Calendar,
+                        text = "2022"
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    MovieGenreChip(
+                        genre = "Action"
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                ElevatedButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Icon(
+                        imageVector = FontAwesomeIcons.Solid.Play,
+                        contentDescription = null,
+                        modifier = Modifier.size(12.dp)
+                    )
+
+                    Text(
+                        text = "Watch Trailer",
+                        modifier = Modifier.padding(start = 16.dp),
+                        fontWeight = FontWeight.Medium,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                BoxWithConstraints(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    val itemWidth = this.maxWidth * 0.55f
+
+                    LazyRow(
+                        contentPadding = PaddingValues(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        items(10) {
+                            CastMemberItem(
+                                profilePictureUrl = "",
+                                name = "Will Smith",
+                                character = "John Smith",
+                                modifier = Modifier.width(itemWidth)
+                            )
+                        }
+                    }
+                }
+                Box(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = "This is just a placeholder text",
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+            }
         }
     }
 }
